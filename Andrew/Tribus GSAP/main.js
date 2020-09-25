cd10d036f6282636cde9ec7685f0866c79f62ee3
ampascu <Andreea.Pascu@oracle.com>
2020-09-25 21:55:31 +0300

    Added extra animations

--- Andrew/Tribus GSAP/main.js
+++ Andrew/Tribus GSAP/main.js
@@ -14,13 +14,19 @@ var full_hook = document.getElementById("full_hook"),
   angular_logo = document.getElementById("angular_logo"),
   react_logo = document.getElementById("react_logo"),
   logos = document.querySelectorAll(".logo"),
+  drop_elements = document.querySelectorAll(".drop"),
+  green_div = document.getElementById("green_div"),
+  underConstruction_text = document.getElementById("under_construction_text"),
+  website_text = document.getElementById("website_text"),
+  bird_left = document.querySelectorAll(".bird_left"),
+  bird_right = document.querySelectorAll(".bird_right"),
   website_tl,
-  logo_tl;
+  birds_tl;
+gsap.registerPlugin(CustomEase);
 
 website_tl = new TimelineMax();
-logo_tl = new TimelineMax();
+birds_tl = new TimelineMax();
 /* Setup stage */
-// boot up the first call
 
 gsap.to(logos, 1, {
   autoAlpha: 1,
@@ -32,7 +38,39 @@ gsap.to(logos, 1, {
   repeat: -1,
 });
 
+gsap.fromTo(
+  clouds_background,
+  30,
+  { x: -2000, ease: "none" },
+  {
+    x: 2000,
+    ease: "none",
+    stagger: {
+      each: 1,
+      repeat: -1,
+    },
+  }
+);
+
+gsap.fromTo(
+  clouds_foreground,
+  20,
+  { x: -2000, ease: "none" },
+  {
+    x: 2000,
+    ease: "none",
+    stagger: {
+      each: 1,
+      repeat: -1,
+    },
+  }
+);
+
+//drop
+gsap.fromTo(drop_elements, 0.5, { y: -150 }, { y: 0, ease: "bounce.out" });
+
 website_tl
+  .add("begin", 0)
   .fromTo(full_hook, 6, { yPercent: -100 }, { yPercent: -10 })
   .fromTo(
     full_hook,
@@ -66,7 +104,7 @@ website_tl
     },
     "-=4"
   )
-  .to(full_hook, 2, {
+  .to(full_hook, 1.5, {
     ease: Power1.easeInOut,
     transformOrigin: "top center",
     rotation: 0,
@@ -76,9 +114,9 @@ website_tl
   .to(hook, 2, { yPercent: -20, ease: Power3.easeInOut }, "-=1")
   .fromTo(
     moto_stiv,
-    4,
+    2,
     { xPercent: 1000 },
-    { xPercent: 0, ease: Power4.easeOut },
+    { xPercent: 0, ease: Power1.easeOut },
     "-=1"
   )
   .to(
@@ -95,4 +133,29 @@ website_tl
     2,
     { rotation: -500, transformOrigin: "50% 50%" },
     "-=2"
+  )
+  .to(
+    green_div,
+    2.5,
+    {
+      rotation: 90,
+      transformOrigin: "0% 0%",
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.14,0 0.209,0.295 0.232,0.42 0.258,0.566 0.246,0.601 0.272,0.75 0.29,0.856 0.308,0.997 0.364,0.997 0.458,0.997 0.398,0.152 0.494,0.152 0.604,0.152 0.516,0.846 0.64,0.846 0.726,0.846 0.696,0.298 0.782,0.298 0.854,0.298 0.792,0.664 0.876,0.664 0.926,0.664 0.939,0.46 0.966,0.46 1.004,0.46 0.999,0.538 1,0.538 "
+      ),
+    },
+    "-=1"
+  )
+  .fromTo(
+    underConstruction_text,
+    1,
+    { autoAlpha: 0, y: -2000 },
+    { autoAlpha: 1, y: 0, ease: "bounce.out" }
+  )
+  .fromTo(
+    website_text,
+    1,
+    { autoAlpha: 0, y: -2000 },
+    { autoAlpha: 1, y: 0, ease: "bounce.out" }
   );
